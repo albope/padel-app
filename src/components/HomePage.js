e336b2050a502075376f19f94d1a1012a165ab19
Alberto Bort <albertobort@gmail.com>
2024-09-10 12:13:52 +0200

    correción duplicidad de elementos

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Button, Typography, Container } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Container } from '@mui/material';
 import ResultsList from './ResultsList'; 
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Últimos partidos
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => navigate('/add-result')}
-      >
-        AÑADIR RESULTADO
-      </Button>
-
-      {/* Asegúrate de que ResultsList solo se renderiza aquí una vez */}
       <ResultsList />
     </Container>
   );
