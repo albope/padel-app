81f0249bbb8d05c8a0bdb15af984deb541702b3a
Alberto Bort <albertobort@gmail.com>
2024-09-10 17:40:35 +0200

    mejoras funcionales

--- src/components/ResultForm.js
+++ src/components/ResultForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography, Grid, Select, MenuItem, InputLabel, FormControl, Box } from '@mui/material';
+import { TextField, Button, Container, Typography, Grid, Select, MenuItem, InputLabel, FormControl, Box, IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 import { collection, addDoc } from "firebase/firestore";
+import DeleteIcon from '@mui/icons-material/Delete'; // Importa el icono de eliminar
 
 const players = ["Lucas", "Ricardo", "Martin", "Bort", "Invitado"];
 const locations = ["Passing Padel", "Elite Padel 22", "Flow Padel", "Aspresso k7", "Otro"];
@@ -38,9 +39,20 @@ const ResultForm = () => {
     }
   };
 
+  // Función para añadir un tercer set
   const addThirdSet = () => {
-    setShowThirdSet(true);
-    setSets([...sets, { pair1Score: '', pair2Score: '' }]);
+    if (!showThirdSet) {
+      setShowThirdSet(true);
+      setSets([...sets, { pair1Score: '', pair2Score: '' }]);
+    }
+  };
+
+  // Función para eliminar el tercer set
+  const removeThirdSet = () => {
+    if (showThirdSet) {
+      setShowThirdSet(false);
+      setSets(sets.slice(0, 2)); // Elimina el tercer set
+    }
   };
 
   return (
@@ -177,17 +189,29 @@ const ResultForm = () => {
       </Grid>
 
       {/* Botón para añadir tercer set */}
-      <Box textAlign="center">
-        <Button 
-          variant="contained" 
-          onClick={addThirdSet}
-          sx={{ backgroundColor: '#800080', color: '#FFFFFF', marginTop: '20px' }}
-        >
-          Añadir Tercer Set
-        </Button>
-      </Box>
+      {!showThirdSet && (
+        <Box textAlign="center">
+          <Button 
+            variant="contained" 
+            onClick={addThirdSet}
+            sx={{ backgroundColor: '#800080', color: '#FFFFFF', marginTop: '20px' }}
+          >
+            Añadir Tercer Set
+          </Button>
+        </Box>
+      )}
 
-      {/* Botones */}
+      {/* Botón para eliminar el tercer set */}
+      {showThirdSet && (
+        <Box textAlign="center" display="flex" justifyContent="center" alignItems="center" sx={{ marginTop: '20px' }}>
+          <Typography variant="body1" sx={{ marginRight: 1 }}>Tercer Set Añadido</Typography>
+          <IconButton onClick={removeThirdSet} sx={{ color: 'red' }}>
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      )}
+
+      {/* Botones de acción */}
       <Grid container spacing={2} style={{ marginTop: '20px' }}>
         <Grid item>
           <Button variant="contained" color="primary" onClick={handleSubmit}>
