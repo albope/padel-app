841bcad7acfa4bf6c0c418136d4f208201884dba
Alberto Bort <albertobort@gmail.com>
2024-09-12 11:05:43 +0200

    Logica partidos consecutivos mejora II

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,17 +5,40 @@ import ResultsList from './ResultsList';
 const HomePage = () => {
   return (
     <Container maxWidth="sm" disableGutters>
-      {/* Encabezado de la App sin fondo negro y con texto negro */}
+      {/* Encabezado de la App con logo y texto estilo minimalista */}
       <Box
         sx={{
-          backgroundColor: 'transparent', // Sin fondo
-          color: 'black', // Texto en negro
+          backgroundColor: 'transparent',
+          color: 'black',
           padding: '20px',
-          textAlign: 'center',
-          fontFamily: '"Oswald", sans-serif', // Nueva fuente
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start', // Alineado a la izquierda
+          fontFamily: '"Roboto", sans-serif', // Fuente moderna
         }}
       >
-        <Typography variant="h4" component="h1">
+        {/* Logo de la raqueta */}
+        <Box
+          component="img"
+          src={`${process.env.PUBLIC_URL}/pelota-de-padel.ico`} // Ruta al logo de la raqueta
+          alt="Raqueta"
+          sx={{
+            height: '40px', // Tamaño del logo ajustado para minimalismo
+            marginRight: '15px', // Espacio adecuado entre logo y texto
+          }}
+        />
+
+        {/* Título más estilizado y minimalista */}
+        <Typography 
+          variant="h5" 
+          component="h1" 
+          sx={{
+            fontWeight: 'bold', 
+            letterSpacing: '1px', 
+            fontSize: '1.8rem', // Tamaño más moderado para un look refinado
+            color: '#333', // Color sutil para el texto
+          }}
+        >
           Padel Mas Camarena
         </Typography>
       </Box>
