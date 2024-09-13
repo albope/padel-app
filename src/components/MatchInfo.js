417306f5ea360f5c2ed816053683fa0d143065d8
Alberto Bort <albertobort@gmail.com>
2024-09-13 09:40:58 +0200

    Se añade integración google calendar

--- src/components/MatchInfo.js
+++ src/components/MatchInfo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Container, Typography, Box, Button } from '@mui/material';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'; // Icono de campana
 import { useNavigate } from 'react-router-dom';
 
 const MatchInfo = () => {
-  const navigate = useNavigate(); // Hook para la navegación
+  const navigate = useNavigate();
+
+  // Enlaces a Google Calendar con recurrencia
+  const calendarLinkTuesday = `https://www.google.com/calendar/render?action=TEMPLATE&text=Partida%20de%20Padel%20-%20Martes&dates=20240917T183000Z/20240917T200000Z&details=Partida%20de%20padel%20en%20Elite%20Padel%2022&location=Elite%20Padel%2022&recur=RRULE:FREQ=WEEKLY;BYDAY=TU`;
+  const calendarLinkThursday = `https://www.google.com/calendar/render?action=TEMPLATE&text=Partida%20de%20Padel%20-%20Jueves&dates=20240919T173000Z/20240919T190000Z&details=Partida%20de%20padel%20en%20Passing%20Padel&location=Passing%20Padel&recur=RRULE:FREQ=WEEKLY;BYDAY=TH`;
 
   return (
     <Container>
@@ -12,7 +17,7 @@ const MatchInfo = () => {
         <Typography variant="h5">Información de las Partidas</Typography>
       </Box>
 
-      {/* Información de la partida de los martes */}
+      {/* Información de la partida del martes */}
       <Box sx={{ marginTop: '20px' }}>
         <Typography variant="h6"><strong>Partida del Martes</strong></Typography>
         <Typography variant="body1">Fecha: Martes, <strong>20:30 - 22:00</strong></Typography>
@@ -30,9 +35,27 @@ const MatchInfo = () => {
             loading="lazy"
           ></iframe>
         </Box>
+
+        {/* Botón de sincronización con Google Calendar */}
+        <Box sx={{ textAlign: 'center', marginTop: '20px' }}>
+          <Button
+            component="a"
+            href={calendarLinkTuesday}
+            target="_blank"
+            startIcon={<NotificationsActiveIcon sx={{ color: 'red' }} />} // Icono de campana en rojo
+            sx={{
+              textTransform: 'none',
+              backgroundColor: '#f0f0f0',
+              color: '#333',
+              '&:hover': { backgroundColor: '#ddd' }
+            }}
+          >
+            Añadir la Partida del Martes a Google Calendar
+          </Button>
+        </Box>
       </Box>
 
-      {/* Información de la partida de los jueves */}
+      {/* Información de la partida del jueves */}
       <Box sx={{ marginTop: '20px' }}>
         <Typography variant="h6"><strong>Partida del Jueves</strong></Typography>
         <Typography variant="body1">Fecha: Jueves, <strong>19:30 - 21:00</strong></Typography>
@@ -50,6 +73,24 @@ const MatchInfo = () => {
             loading="lazy"
           ></iframe>
         </Box>
+
+        {/* Botón de sincronización con Google Calendar */}
+        <Box sx={{ textAlign: 'center', marginTop: '20px' }}>
+          <Button
+            component="a"
+            href={calendarLinkThursday}
+            target="_blank"
+            startIcon={<NotificationsActiveIcon sx={{ color: 'red' }} />} // Icono de campana en rojo
+            sx={{
+              textTransform: 'none',
+              backgroundColor: '#f0f0f0',
+              color: '#333',
+              '&:hover': { backgroundColor: '#ddd' }
+            }}
+          >
+            Añadir la Partida del Jueves a Google Calendar
+          </Button>
+        </Box>
       </Box>
 
       {/* Botón para volver a la pantalla principal */}
@@ -66,7 +107,7 @@ const MatchInfo = () => {
               backgroundColor: '#333',
             },
           }}
-          onClick={() => navigate('/')} // Navega de vuelta a la pantalla principal
+          onClick={() => navigate('/')}
         >
           Volver a la pantalla principal
         </Button>
