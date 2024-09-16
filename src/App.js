e5c5368216b86d4cfb28e3aff3da9c521195dbdd
albope <albertobort@gmail.com>
2024-09-16 17:40:55 +0200

    Añadir cambios en App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ResultForm from './components/ResultForm';
-import StatsChart from './components/StatsChart';
 import MatchInfo from './components/MatchInfo';
 import Players from './components/Players';
 import Insignias from './components/Insignias';
@@ -18,7 +17,6 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/add-result" element={<ResultForm />} />
-            <Route path="/stats" element={<StatsChart />} />
             <Route path="/info" element={<MatchInfo />} />
             <Route path="/players" element={<Players />} />
             <Route path="/insignias" element={<Insignias />} />
